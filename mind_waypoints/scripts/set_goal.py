7e4565dfc591ab72a796ed64668bf3e7f7fbdac5
Asser M. Zayed <asserzayed@gmail.com>
2022-07-22 17:45:27 +0200

    v3

--- mind_waypoints/scripts/set_goal.py
+++ mind_waypoints/scripts/set_goal.py
@@ -43,7 +43,7 @@ def done_cb(status, result):
 	if status == 4:
 		rospy.loginfo("Goal aborted")
 
-def caller():
+def caller(target):
 	rospy.loginfo(waypoints)
 	navclient = actionlib.SimpleActionClient('move_base',MoveBaseAction)
 	navclient.wait_for_server()
@@ -52,31 +52,29 @@ def caller():
 	# Read the .csv file
 	with open(output_file_path, 'r') as file:
 		reader = csv.reader(file, delimiter = ',')
-		for target in waypoints:
-			for row in reader:
-				#print (row)
-				if(row[0] == target):
-					goal.target_pose.header.frame_id = "map"
-					goal.target_pose.header.stamp = rospy.Time.now()
-					goal.target_pose.pose.position.x = float(row[1])
-					goal.target_pose.pose.position.y = float(row[2])
-					goal.target_pose.pose.position.z = 0.0
-					goal.target_pose.pose.orientation.x = 0.0
-					goal.target_pose.pose.orientation.y = 0.0
-					#goal.target_pose.pose.orientation.z = 0.662
-					#goal.target_pose.pose.orientation.w = 0.750
-					goal.target_pose.pose.orientation.z = 0.0
-					goal.target_pose.pose.orientation.w = 1.0
-					rospy.loginfo("Your goal is being sent!\n")
-					navclient.send_goal(goal, done_cb, active_cb, feedback_cb)
-					finished = navclient.wait_for_result()
-					
+		for row in reader:
+			#print (row)
+			if(row[0] == target):
+				goal.target_pose.header.frame_id = "map"
+				goal.target_pose.header.stamp = rospy.Time.now()
+				goal.target_pose.pose.position.x = float(row[1])
+				goal.target_pose.pose.position.y = float(row[2])
+				goal.target_pose.pose.position.z = 0.0
+				goal.target_pose.pose.orientation.x = 0.0
+				goal.target_pose.pose.orientation.y = 0.0
+				#goal.target_pose.pose.orientation.z = 0.662
+				#goal.target_pose.pose.orientation.w = 0.750
+				goal.target_pose.pose.orientation.z = 0.0
+				goal.target_pose.pose.orientation.w = 1.0
+				rospy.loginfo("Your goal is being sent!\n")
+				navclient.send_goal(goal, done_cb, active_cb, feedback_cb)
+				finished = navclient.wait_for_result()
+				
 ############################################################################################
-					if not finished:
-						rospy.logerr("Action server not available!")
-					else:
-						rospy.loginfo ( navclient.get_result())
-	controller()
+				if not finished:
+					rospy.logerr("Action server not available!")
+				else:
+					rospy.loginfo ( navclient.get_result())
 	
 def controller():
 		# init the node
@@ -101,14 +99,19 @@ def controller():
 				something=input()
 				waypoints.append(something)
 				print(waypoints)
-			caller()
+			for target in waypoints:
+				caller(target)
 		elif inp =='2':
 			waypoints=[]
 			with open(output_file_path, 'r') as file:
 				reader = csv.reader(file, delimiter = ',')
 				for row in reader:
-					waypoints.append(row[0])
-			caller()
+					if(len(row[0])>2):
+						pass
+					else:
+						waypoints.append(row[0])
+			for target in waypoints:
+				caller(target)
 			
 		elif inp =='3':
 			waypoints=[]
